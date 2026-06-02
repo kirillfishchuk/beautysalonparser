0b86f97f3d76fdb2b40e841495a1b9b20cf05b0b
kirichfisher <kirill.fishchuk@gmail.com>
2026-06-03 00:55:15 +0200

    initialized repo

---
+++ src/main/java/com/kirichfisher/beautysalonparser/BeautySalonParserApplication.java
@@ -0,0 +1,13 @@
+package com.kirichfisher.beautysalonparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalonParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautySalonParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kirichfisher/beautysalonparser/BeautySalonParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirichfisher.beautysalonparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySalonParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
